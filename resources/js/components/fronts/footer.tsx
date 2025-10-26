96c688ed6ac7f3a0254042c39b2b50b2458cce65
Mamadou Lamine Diallo <sodioboutique@gmail.com>
2025-10-26 14:37:10 +0000

    Add laravel envoy

--- resources/js/components/fronts/footer.tsx
+++ resources/js/components/fronts/footer.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 export default function Footer() {
     return (
         <footer className="bg-slate-50 py-16 dark:bg-slate-900">
-            <div className="mx-auto max-w-7xl">
+            <div className="mx-auto max-w-7xl px-4 md:px-0">
                 <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
                     <div className="flex flex-col gap-2 md:col-span-2">
                         <Link href="" className="flex items-center">
